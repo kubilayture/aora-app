0f6a61dcd349954a459eeebc26a39497dda90991
Kubilay Türe <kubilayture@gmail.com>
2024-08-04 17:48:28 +0300

    home screen

---
+++ lib/useAppwrite.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+
+const useAppwrite = (fn: Function) => {
+  const [data, setData] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fn();
+      setData(response);
+    } catch (error: any) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => fetchData();
+  return { data, refetch, isLoading };
+};
+
+export default useAppwrite;
